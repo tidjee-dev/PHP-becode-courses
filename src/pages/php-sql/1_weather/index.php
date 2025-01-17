cebd10d67a52be8d85f6b996a0535c9942fa92c8
Donatien Pinet <pinet.donatien@gmail.com>
2025-01-17 17:28:32 +0100

    ‼️ BREAKING: move index.php to src/pages/php-sql/1_weather

--- src/pages/php-sql/1_weather/index.php
+++ src/pages/php-sql/1_weather/index.php
@@ -9,8 +9,6 @@ $errorMessage = [
   "table" => ""
 ];
 
-
-
 try {
   $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
